209fdaa351d3bf9dc92354b66bdd1740a9f7770a
Danil Filimonuyk <danil.filimonuyk@yandex.ru>
2024-07-27 19:16:58 +0300

    Первый коммит проекта 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+let cardTEmplate = document.querySelector('#card-template').content; // То, что я буду вставлять в элемент .places_list
 
 // @todo: DOM узлы
+let placesList = document.querySelector('.places__list');// сам элемент .places__list 
 
 // @todo: Функция создания карточки
+function cardElementcreation(item, cardElementRemoval){ 
+    let cardElement = cardTEmplate.querySelector('.places__item').cloneNode(true);// Скопировали
+    
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__image').alt = item.name;
+    cardElement.querySelector('.card__title').textContent = item.name;
+    let ButtonRemoval = cardElement.querySelector('.card__delete-button');
+    ButtonRemoval.addEventListener('click', function () {
+        cardElementRemoval(cardElement);
+        });
+    return cardElement;
+}    
 
 // @todo: Функция удаления карточки
-
+ function cardElementRemoval(card) { 
+    card.remove();
+ }
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+    let card = cardElementcreation(item, cardElementRemoval);
+    placesList.append(card);
+   
+}); 
